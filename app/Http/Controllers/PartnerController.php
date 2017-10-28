d216a36486e070077827aebb09cb55ec39f42378
Ashish Rana <ashisharana@yahoo.com>
2017-10-28 22:00:22 +0530

    partner route

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -4,24 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ModelController extends Controller
+class PartnerController extends Controller
 {
     //
 
-    public function profile($lang){
-
-
-    	return view('model.profile');
-
-    }
-
     public function register($lang){
 
-    	return view("model.register");
-    }
-
-      public function model_prize($lang){
-
-    	return view("model.model_prize");
+    	return view("partner.register");
     }
 }
